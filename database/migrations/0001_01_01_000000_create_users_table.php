a1f7448eafe3ddca7b4967eb9afddb6e9b0651ee
ouzy012 <ousmane.ndiaye8518@gmail.com>
2025-02-17 19:00:33 +0000

    create project

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,18 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('prenom');
+            $table->string('nom');
+            $table->string('telephone')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('profil',
+                [
+                    'SUPER_ADMIN',
+                    'GERANT',
+                    'PARTICIPANT'
+                ])->default('PARTICIPANT');
             $table->rememberToken();
             $table->timestamps();
         });
